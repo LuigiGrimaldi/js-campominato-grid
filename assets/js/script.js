cf3a840c565ea47483026c6687f6c47493c9346f
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-19 21:30:54 +0100

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+
+const containe = document.querySelector(".containe");
+
+// creo un max numero di celle
+
+let nMax = 100;
+
+// creo un ciclo n volte per le celle (dove n in questo caso sta per 64)
+
+for (let i = 0; i < nMax; i++) {
+  const cell = `<div class="cell"></div>`;
+  containe.innerHTML += cell;
+}
+
+// seleziono una cella che ha classe cell e active
+
+const cellEl = document.querySelectorAll(".cell")
+
+// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+
+
+for (let i = 0; i < cellEl.length; i++) {
+    const thisCell = cellEl[i];
+    console.log(thisCell)
+    thisCell.addEventListener("click", function() {
+        thisCell.classList.toggle("bg_green")
+        // this.classList.add("active")
+        console.log("Changed the color")
+    })
+
+
+    
+}
